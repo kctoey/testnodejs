0d95c6c45616075ff782271ca53b516e0f945c42
kctoey <95272813+kctoey@users.noreply.github.com>
2023-03-15 11:18:28 +0700

    update template

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const debug = require("debug")("app");
 const app = express();
 const port = 3000;
 const morgan = require("morgan");
+const path = require("path");
 app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 app.get("/", (req, res) => {
-  res.send("Hello world mama");
+  res.send("Not Found Index file");
 });
 
 app.listen(port, () => {
