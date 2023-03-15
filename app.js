bb8d0cb69f212602f08a9fa235d0457a2ce8e6aa
kctoey <95272813+kctoey@users.noreply.github.com>
2023-03-15 15:59:41 +0700

    update page

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
+
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
-app.get("/", (req, res) => {
-  res.send("Not Found Index file");
-});
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "Lnwza55",
+    customers: ["John L.", "Frank", "Leon"],
+  });
+});
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 app.listen(port, () => {
   debug("Listening on port" + chalk.red(":" + port));
-  console.log(chalk.blue("Hello world!"));
+  console.log(chalk.blue("Hello Fucking world!!!"));
 });
