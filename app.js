47afadf42c7f599c82d9f6ec8efc2df4e8cd4487
kctoey <95272813+kctoey@users.noreply.github.com>
2023-03-15 16:08:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
+const productRouter = express.Router();
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
-
-app.get("/products");
+productRouter.router("/").get((req, res) => {
+  res.send("Welcome to shop");
+});
+productRouter.router("/").get((req, res) => {
+  res.send("T-Shrit");
+});
+app.get("/products", productRouter);
 app.get("/", (req, res) => {
   res.render("index", {
     username: "Lnwza55",
