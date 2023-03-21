faaba6566d86c00deca26f697f37fffeee32546e
kctoey <95272813+kctoey@users.noreply.github.com>
2023-03-21 15:34:51 +0700

    up

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router();
 
+const app = express();
+const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter");
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
-productRouter.route("/").get((req, res) => {
-  res.render("products", { products });
-});
-
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.render("product", {
-    product: products[id],
-  });
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
